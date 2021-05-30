869188d3af07ea9edb2d6001ae2e008423955877
hiro <hmaeyama0510@gmail.com>
2021-05-30 10:27:51 +0900

    add axios and create types api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { UserCard } from "./components/UserCard";
+import { User } from "./types/api/user";
 import "./styles.css";
 
 const user = {
@@ -9,8 +11,17 @@ const user = {
 };
 
 export default function App() {
+  const onClickFetchData = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   return (
     <div className="App">
+      <button onClick={onClickFetchData}>Get Data</button>
       <UserCard user={user} />
     </div>
   );
