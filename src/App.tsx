faf9624bafff05abc8b3197ef8a011b90dc48afb
hiro <hmaeyama0510@gmail.com>
2021-05-30 11:31:41 +0900

    Create perfect custom hook!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-import axios from "axios";
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
+import { useAllUsers } from "./hooks/useAllUsers";
 import "./styles.css";
-import { useState } from "react";
-import { UserProfile } from "./types/userProfile";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getusers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchData = () => {
-
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      }).catch(() => {
-        setError(true);
-      }).finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchData = () => getusers();
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get Data</button>
-      <br/>
+      <br />
       {error ? (
         <p style={{ color: "red" }}>Failed. Try it again.</p>
       ) : loading ? (
