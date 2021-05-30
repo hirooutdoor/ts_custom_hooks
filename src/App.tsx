31b2086a7028ba268b07730b756f991140040808
hiro <hmaeyama0510@gmail.com>
2021-05-30 10:44:17 +0900

    create  useState for userProfiles

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,32 @@ import axios from "axios";
 import { UserCard } from "./components/UserCard";
 import { User } from "./types/api/user";
 import "./styles.css";
-
-const user = {
-  id: 1,
-  name: "Armin",
-  email: "thisis@aaaaaaa.aaaa",
-  address: "hometowm, japan"
-};
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
   const onClickFetchData = () => {
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
-        console.log(res.data);
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
       });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get Data</button>
-      <UserCard user={user} />
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
